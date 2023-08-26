a7c9dc142f353747c28e593ee21fb81098e91c0b
treesaretall <himatthewhurst@hotmail.com>
2023-08-26 13:30:13 -0400

    putDb created

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,20 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => try {
+  console.log('Connecting to putDb');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log('Data saved to the database', result);
+} catch (error) {
+  console.error('putDb not implemented', error);
+}
+  
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
